f29525ce4a718eb418375551fd4875f4b553e3e6
Erjoy Constantine L. Robles <elrobles1@up.edu.ph>
2025-03-13 16:25:12 +0800

    feat: working create user and get all users

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,9 +1,6 @@
-import { connectDB } from "@/app/services/database/database";
+
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  await connectDB();
+export async function GET(req: Request) {
   return NextResponse.json({ message: "This is the change that has been made" });
 }
-
-
---
+++ src/app/services/user/userService.ts
@@ -0,0 +1,47 @@
+import { connectDB } from "@/app/services/database/database";
+import { UserModel, IUser } from "@/models/user_model";
+
+async function getAllUsers() {
+  console.log("Fetching all users.");
+  
+  await connectDB().then(
+    () => console.log("Successfully connected to the database.")
+  ).catch(
+    () => console.log("There was an error with connecting to the database.")
+  )
+
+  let results = await UserModel.find();
+
+  return results;
+}
+
+async function createUser(user: IUser) {
+  console.log("Creating a user.");
+
+
+  try {
+    await connectDB().catch(
+      (error) => {
+        console.log("There was an error.", error)
+      }
+    );
+    let created_user = await UserModel.create(user)
+    return {
+      success: true,
+      data: created_user
+    }
+
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      data: error
+    }
+  }
+  // console.log(user)
+}
+
+export {
+  getAllUsers,
+  createUser
+};
