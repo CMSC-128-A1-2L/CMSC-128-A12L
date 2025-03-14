fe3d3b94b19e0bc3c3a2d29a12c323561048929d
klenndacoder69 <kvborja@up.edu.ph>
2025-03-14 18:07:17 +0800

    docs: documentation for the object properties of OAuth2.0

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 /*
  * This file handles incoming authentication requests in authentication (auth/[...nextauth]/route.ts)
  * any routes that goes here, goes to route.ts
- *
  * */
 
 import NextAuth, { NextAuthOptions } from "next-auth";
@@ -20,10 +19,10 @@ const googleClientSecret = process.env.CLIENT_SECRET ?? (() => {
 // Introducing NextAuthOptions type-safety reduces the need to include types in parameters in callbacks
 const authOptions: NextAuthOptions = {
   providers: [
-    GoogleProvider({
+      GoogleProvider({
       clientId: googleClientId,
       clientSecret: googleClientSecret,
-      // this authorization parameter makes it so that every time the user logs in the refresh token is given always
+      // This authorization parameter makes it so that every time the user logs in the refresh token is given always
       authorization: {
         params: {
           prompt: "consent",
@@ -33,23 +32,43 @@ const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  // This part indicates the maximum time before the session is removed
+  session: {
+    strategy: "jwt",
+    maxAge: 10,
+  },
   callbacks: {
-    async session({ session, user }) {
-      console.log("Session: ", session);
-      console.log("User: ", user);
+        /*
+        the session contains the following properties:
+     * - name: string (User's full name)
+     * - email: string (User's email address)
+     * - image: string (URL to user's profile picture)
+     * - expires: string (Expiration timestamp)
+     */
+    async session({ session }) {
       return session;
     },
     async signIn({ user, account, profile }) {
-      // account contains 'refresh token'
-      console.log("Sign in: ", { user, account, profile })
+      /* 
+      The account contaisn the following properties:
+      - provider: string (Google)
+      - refresh_token: string (A refresh token)
+      - token_type: string (usually the Bearer) 
+      - expires_at: number (Expiration time in seconds) 
+      */ 
       return true;
     },
+        /*
+     * - The token object contains:
+     *   - id: string (User ID)
+     *   - email: string (User's email)
+     *   - accessToken: string (OAuth access token)
+     */
     async jwt({ token, user, account }) {
       if (account && user) {
         token.id = user.id;
         token.email = user.email;
         token.accessToken = account.access_token;
-        console.log("The token contents are: ", token);
       }
       return token;
     }
