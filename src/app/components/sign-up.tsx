ba49644a2725cf01043398f19cd37e17a00d1e06
Jhin Fernandez <jvfernandez4@up.edu.ph>
2025-03-17 00:25:55 +0800

    chore: added form validation

--- src/app/components/sign-up.tsx
+++ src/app/components/sign-up.tsx
@@ -1,18 +1,86 @@
+"use client";
+import { useState } from 'react';
+
 export default function SignUp() {
+
+    const[formInput, setInput] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    const [formError, setError] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const handleInput = (name, value) => {
+        setInput({
+            ...formInput,
+            [name]: value
+        });
+    }
+
+    const validateInput = (event) =>{
+        event.preventDefault();
+
+        let formError = {
+            email: "",
+            password: "",
+            confirmPassword: ""
+        }
+        
+        if (!formInput.email.match("[a-z0-9._%+-]+@[a-z0-9]+\.[a-z]{2,}$") && formInput.password !== formInput.confirmPassword){
+            setError({
+                ...formError,
+                email: "Enter valid email address",
+                confirmPassword: "Password and confirm password should match"
+            })
+            return
+        }
+
+        if (!formInput.email.match("[a-z0-9._%+-]+@[a-z0-9]+\.[a-z]{2,}$")){
+            setError({
+                ...formError,
+                email: "Enter valid email address"
+            })
+            return
+        }
+
+        if (formInput.password !== formInput.confirmPassword){
+            setError({
+                ...formError,
+                confirmPassword: "Password and confirm password should match"
+            })
+            return
+        }
+
+        setError(formError);
+        setInput((prevState) => ({
+            ...prevState
+        }))
+    }
     return (
         <div className="h-screen flex items-center justify-center">
             <div className="card bg-base-300 w-110">
                 <div className="card-body items-center">
                     <h2 className="card-title">Create Alumni Account</h2>
-                    <form>
-                        <div className="card-body items-center"> 
+                    <form onSubmit={validateInput}>
+                        <div className="card-body"> 
                             <div className="join mb-2 gap-2 w-84">
-                                <input type="text" className="input" placeholder="First Name" />
-                                <input type="text" className="input" placeholder="Last Name" />
+                                <input type="text" className="input" placeholder="First Name" required />
+                                <input type="text" className="input" placeholder="Last Name" required />
                             </div>
-                            <input type="email" className="input mb-2 w-84" placeholder="Email" />
-                            <input type="password" className="input mb-2 w-84" placeholder="Password" />
-                            <input type="password" className="input mb-2 w-84" placeholder="Confirm Password" />
+
+                            <input type="text" onChange={({ target }) => {handleInput(target.name, target.value)}} name="email" className="input mb-2 w-84" placeholder="Email" required />
+                            {formError.email && <span className="mb-3 text-sm text-red-400">{formError.email}</span>}
+
+                            <input value={formInput.password} onChange={({ target }) => {handleInput(target.name, target.value)}} name="password" type="password" className="input mb-2 w-84" placeholder="Password" required />
+
+                            <input value={formInput.confirmPassword} onChange={({ target }) => {handleInput(target.name, target.value)}} name="confirmPassword" type="password" className="input mb-2 w-84" placeholder="Confirm Password" required />
+                            {formError.confirmPassword && <span className="text-sm text-red-400">{formError.confirmPassword}</span>}
+
                         </div>
                         <div className="card-actions justify-center">
                             <button className="btn btn-wide btn-neutral-content">Sign Up</button>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,9 @@
 import SignUp from "@/app/components/sign-up";
 
 export default function Registration() {
-	return (
-    <div> 
-      <SignUp/>
-    </div>
-  )
+    return (
+        <div> 
+            <SignUp/>
+        </div>
+    )
 }
