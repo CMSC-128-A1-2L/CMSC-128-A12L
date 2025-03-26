f0c9014212453c77be789b5554d1523dca3b5738
code_klenn <kvborja@up.edu.ph>
2025-03-26 15:08:24 +0800

    fix: removed some turbopack issues in naming conventions, added an 'any' type in event parameter

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -23,7 +23,7 @@ export default function UsersManagement(){
     };
 
     // search input box only works for when user presses enter key
-    const handleSearch = (e) => {
+    const handleSearch = (e: any) => {
         if (e.key === "Enter") {  // Check if Enter key is pressed
             setSearchQuery(tempQuery);
         }
@@ -71,7 +71,7 @@ export default function UsersManagement(){
         <div className="relative w-full sm:w-auto">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
             </div>
             <input  type="search" 
