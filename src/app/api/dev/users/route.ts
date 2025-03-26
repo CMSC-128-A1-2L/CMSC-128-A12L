a2f7753317955ed1223158824a663c1ed9d2cd92
John Rufo de la Peña <jodelapena2@up.edu.ph>
2025-03-26 23:17:10 +0800

    chore: Remove dev route implementation
    Dummy data requires updating after changes to the user schema.

--- src/app/api/dev/users/route.ts
+++ src/app/api/dev/users/route.ts
@@ -1,69 +1,13 @@
-import { connectDB } from "@/app/services/database/database";
 import { NextRequest, NextResponse } from "next/server";
-import { createUser, editUser } from "@/app/services/user/userService";
-import { IUser, UserModel } from "@/models/user";
 import data from "@/dummy_data/user.json"
-import { Types } from "mongoose";
 
 // Populate database endpoint
 export async function POST(req: NextRequest) {
   console.log("[DEV] Triggered populate user collection endpoint.");
-
-  try {
-
-    let originalLog = console.log();
-    (console as any).log = () => {}
-
-    data.forEach(async (element) => {
-      let user = element as unknown as IUser;
-      user.adviser = new Types.ObjectId(element.adviser["$oid"]);
-      user.currentAddress = new Types.ObjectId(element.currentAddress["$oid"]);
-
-      await createUser(user);
-    });
-
-    (console as any).log = originalLog;
-
-    return NextResponse.json({
-      success: true,
-      message: "Successfully populated the user collection."
-    });
-    
-
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({
-      success: false,
-      message: "There was an error with populating the user collection.."
-    });
-  }
+  // Currently left unimplemented. Dummy data needs implementing.
 }
 
 export async function DELETE(req: NextRequest) {
   console.log("[DEV] Triggered clear user collection endpoint.");
-
-  
-
-  try {
-    await connectDB().catch(
-      (error) => {
-        console.log("There was an error with connecting to the database.", error)
-      }
-    );
-    
-    let deleted_users = await UserModel.deleteMany({});
-
-    return NextResponse.json({
-      success: true,
-      message: "Successfully cleared the user collection."
-    });
-    
-
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({
-      success: false,
-      message: "There was an error with clearing the user collection."
-    });
-  }
+  // Currently left unimplemented. Dummy data needs implementing.
 }
