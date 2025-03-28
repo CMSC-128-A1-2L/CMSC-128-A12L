b5aee7dc536fe1a715720819d39f76d3c8b9abda
John Rufo de la Peña <jodelapena2@up.edu.ph>
2025-03-28 21:49:03 +0800

    chore: Update admin API to unified auth handling

--- src/app/api/admin/users/[id]/route.ts
+++ src/app/api/admin/users/[id]/route.ts
@@ -1,19 +1,47 @@
-import { connectDB } from "@/app/services/database/database";
 import { NextRequest, NextResponse } from "next/server";
-import { createUser, deleteUser, editUser } from "@/app/services/user/userService";
-import { IUser } from "@/models/user";
+import { getUserRepository } from "@/repositories/user_repository";
+import { User } from "@/entities/user";
+
+// Get user with specified id
+export async function GET(req: NextRequest,  { params }: { params: { id: string } }) {
+  console.log("Get user endpoint has been triggered.");
+
+  const {id} = await params;
+
+  const userRepository = getUserRepository();
+  const user = await userRepository.getUserById(id);
+
+  if (user === null) {
+    const response = new NextResponse("User not found", { status: 404 });
+    return response;
+  }
+
+  return new NextResponse(JSON.stringify(user), { status: 200 });
+}
 
 // Edit user endpoint
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
   console.log("Edit user endpoint has been triggered.");
 
-  let {id} = await params;
-  console.log(id);
-  let user = await req.json();
-  console.log(user);
+  const {id} = await params;
 
-  let edited_user = await editUser(id, user);
-  return NextResponse.json(edited_user);
+  const userRepository = getUserRepository();
+  const user = await userRepository.getUserById(id);
+  
+  if (user === null) {
+    const response = new NextResponse("User not found", { status: 404 });
+    return response;
+  }
+
+  const body = await req.json();
+  const updatedUser: User = {
+    ...user,
+    ...body
+  }
+
+  return await userRepository.updateUser(updatedUser)
+    .then(() => new NextResponse(JSON.stringify(updatedUser), { status: 200 }))
+    .catch(() => new NextResponse("Failed to update user", { status: 500 }));
 }
 
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
@@ -22,6 +50,9 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
   let {id} = await params;
   console.log(id);
 
-  let deleted_user = await deleteUser(id);
-  return NextResponse.json(deleted_user);
+  const userRepository = getUserRepository();
+
+  return await userRepository.deleteUser(id)
+    .then(() => new NextResponse(null, { status: 204 }))
+    .catch(() => new NextResponse("Failed to delete user", { status: 500 }));
 }
