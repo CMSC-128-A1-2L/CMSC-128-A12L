3d4ccec3e68c67106afaecc5954cfa5a7176af42
Jhin Fernandez <jvfernandez4@up.edu.ph>
2025-04-12 16:00:25 +0800

    chore: modified details of modal

--- src/app/components/sponsorshipsModal.tsx
+++ src/app/components/sponsorshipsModal.tsx
@@ -20,15 +20,18 @@ const SponsorDetails: React.FC<SponsorDetailsProps> = ({
         <h3 className="font-bold text-lg mt-4 text-center">Sponsorship Details</h3>
 
         <div className="pt-4">
-            <p className="font-bold text-left text-gray-700">Contact Person</p>
-            <p className="px-2 pb-2">Lorem Ipsum</p>
+            <p className="font-bold text-left text-gray-700">Contact Details</p>
+            <div className="flex">
+              <p className="px-2 font-bold text-gray-700">Contact Number:</p> 
+              <p>Lorem Ipsum</p>
+            </div>
+            <div className="flex">
+              <p className="px-2 font-bold text-gray-700">Email:</p> 
+              <p className="pb-2">Lorem Ipsum</p>
+            </div>
 
-            <p className="font-bold text-left text-gray-700">Items to Donate</p>
-            <li className="px-2"> Cash </li>
-            <li className="px-2"> Laptop </li>
-
-            <p className="font-bold text-left text-gray-700">Method of Payment</p>
-            <li className="px-2"> Gcash </li>
+            <p className="font-bold text-left text-gray-700">Sponsorship Options</p>
+            <li className="px-2"> Lorem Ipsum </li>
         </div>
       </div>
       <form method="dialog" className="modal-backdrop">
