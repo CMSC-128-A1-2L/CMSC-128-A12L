0c07aa10b87fb29897dac8a077ea2406592b7a58
John Rufo de la Peña <jodelapena2@up.edu.ph>
2025-04-18 11:12:54 +0800

    feat: Create initial password change verifier repository getter

--- src/repositories/change_password_verifier_repository.ts
+++ src/repositories/change_password_verifier_repository.ts
@@ -5,3 +5,19 @@ export interface ChangePasswordVerifierRepository {
     getVerifierByUserId(userId: string): Promise<ChangePasswordVerifier | null>;
     deleteVerifier(verifier: ChangePasswordVerifier): Promise<void>;
 };
+
+export function getChangePasswordVerifierRepository(): ChangePasswordVerifierRepository
+{
+    // TODO: Implement with a proper repository
+    return {
+        async createVerifier(verifier: ChangePasswordVerifier): Promise<void> {
+            throw new Error("Not implemented");
+        },
+        async getVerifierByUserId(userId: string): Promise<ChangePasswordVerifier | null> {
+            throw new Error("Not implemented");
+        },
+        async deleteVerifier(verifier: ChangePasswordVerifier): Promise<void> {
+            throw new Error("Not implemented");
+        }
+    }
+}
